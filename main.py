edfa000ca8ea7f692b43aecb0473d2cdf9f23625
meesamamir <meesam_ameer@hotmail.com>
2022-02-14 14:49:06 -0600

    test2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-#tri & meesam
+#tri & meesamA
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "asdcbsxnxacdaskchds" #encrypts cookies and session data related to website, it can be whatever we want
 
