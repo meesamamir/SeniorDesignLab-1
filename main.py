970fef2d8e7809f301b7259466eef62e2b0ad320
meesamamir <meesam_ameer@hotmail.com>
2022-02-14 14:27:47 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "asdcbsxnxacdaskchds" #encrypts cookies and session data related to website, it can be whatever we want
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/help")
+def help():
+    return render_template("help.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  # update flask server with changes we make
